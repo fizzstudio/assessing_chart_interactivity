11ca31f640af006026680b5fd971bbe0fe3a9d49
Frank Elavsky <frankelavsky@gmail.com>
2022-04-13 19:05:25 -0400

    convert line chart into sparkline

--- src/charts.js
+++ src/charts.js
@@ -1,71 +1,81 @@
 props = {
-  'line-chart': {
-    valueAccessor: 'value',
-    ordinalAccessor: 'date',
-    seriesAccessor: 'category',
-    uniqueID: 'sparkline',
-    data: [
-      {
-        value: 12,
-        date: 'Jan',
-        category: 'Product'
-      },
-      {
-        value: 13,
-        date: 'Feb',
-        category: 'Product'
-      },
-      {
-        value: 15,
-        date: 'Mar',
-        category: 'Product'
-      },
-      {
-        value: 14,
-        date: 'Apr',
-        category: 'Product'
-      },
-      {
-        value: 14,
-        date: 'May',
-        category: 'Product'
-      },
-      {
-        value: 18,
-        date: 'Jun',
-        category: 'Product'
-      },
-      {
-        value: 17,
-        date: 'Jul',
-        category: 'Product'
-      },
-      {
-        value: 19,
-        date: 'Aug',
-        category: 'Product'
-      },
-      {
-        value: 24,
-        date: 'Sep',
-        category: 'Product'
-      },
-      {
-        value: 23,
-        date: 'Oct',
-        category: 'Product'
-      },
-      {
-        value: 29,
-        date: 'Nov',
-        category: 'Product'
-      },
-      {
-        value: 39,
-        date: 'Dec',
-        category: 'Product'
-      }
-    ]
+    'line-chart': {
+        mainTitle: '',
+        subTitle: '',
+        valueAccessor: 'value',
+        ordinalAccessor: 'date',
+        seriesAccessor: 'category',
+        uniqueID: 'sparkline',
+        width: 36,
+        height: 18,
+        padding: { top: 0, bottom: 0, left: 0, right: 0},
+        margin: { top: 0, bottom: 0, left: 0, right: 0},
+        dataLabel: {visible: false},
+        xAxis: {visible: false},
+        yAxis: {visible: false},
+        showDots: false,
+        data: [
+        {
+            value: 12,
+            date: 'Jan',
+            category: 'Product'
+        },
+        {
+            value: 13,
+            date: 'Feb',
+            category: 'Product'
+        },
+        {
+            value: 15,
+            date: 'Mar',
+            category: 'Product'
+        },
+        {
+            value: 14,
+            date: 'Apr',
+            category: 'Product'
+        },
+        {
+            value: 14,
+            date: 'May',
+            category: 'Product'
+        },
+        {
+            value: 18,
+            date: 'Jun',
+            category: 'Product'
+        },
+        {
+            value: 17,
+            date: 'Jul',
+            category: 'Product'
+        },
+        {
+            value: 19,
+            date: 'Aug',
+            category: 'Product'
+        },
+        {
+            value: 24,
+            date: 'Sep',
+            category: 'Product'
+        },
+        {
+            value: 23,
+            date: 'Oct',
+            category: 'Product'
+        },
+        {
+            value: 29,
+            date: 'Nov',
+            category: 'Product'
+        },
+        {
+            value: 39,
+            date: 'Dec',
+            category: 'Product'
+        }
+        ]
   }
 };
 
@@ -77,9 +87,10 @@ const chartConstructor = (chartTag, id) => {
   Object.keys(props[chartTag]).forEach(prop => {
     chart[prop] = props[chartTag][prop];
   });
-  document.body.appendChild(chart);
   return chart;
 };
 const line = chartConstructor('line-chart', props['line-chart'].uniqueID);
+
+document.getElementById("line-insert").appendChild(line)
 
 console.log('hi', line);
